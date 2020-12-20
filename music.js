0937cff1b063a5dc34e65e6af4c7bc848c468393
Spinmeister <Spinmeisterr@users.noreply.github.com>
2020-12-21 00:43:55 +0100

    add Embed

--- music.js
+++ music.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 client.on('ready', () => {
@@ -7,35 +8,43 @@ client.on('ready', () => {
 
   prefix="!!"
   client.on('message', async msg => {
-    const args = msg.content.slice(prefix.length).trim().split(' ');
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    let connextion = await msg.member.voice.channel.join()
+    let Channel_check = await msg.member.voice.channel 
+    let  connection = await Channel_check.join()
+
+    
+
     
 
 if(command==="join") {
 
 
-    if(!msg.member.voice.channel) return msg.reply ("Vous devez être dans channel vocal.")
+    if(!Channel_check ) return msg.reply ("Vous devez être dans channel vocal.")
     
-    connextion
+     connection.join()
+     connection.setDeaf(true)
     msg.channel.send(`Joined ${msg.member.voice.channel.name}`)
     
 
 }
 if(command==="play") {
-    if(!msg.member.voice.channel) return msg.reply ("Vous devez être dans channel vocal.")
- 
+    if(!Channel_check) return msg.reply ("Vous devez être dans channel vocal.")
+  
+    connection.voice.setDeaf(true)
     console.log(args[0])
     const info_music = await ytdl.getInfo(args[0])
-
     const stream  = ytdl(args[0], {filter: 'audioonly'});
- 
-    connextion.play(stream, {seek: 0, volume: 1})
-    msg.channel.send(`now playing ***${info_music.videoDetails.title}***`)
+    
+     connection.play(stream)
+    const emebed = new Discord.MessageEmbed()
+    .setTitle(`now playing ***${info_music.videoDetails.title}***`)
+    .setURL(`${info_music.videoDetails.video_url}`)
+    msg.channel.send(emebed)
 
-}
-})  
 
+} 
+})
 
 
 client.login(token);  
