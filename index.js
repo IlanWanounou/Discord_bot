79bd62a55cf58523fbb89ca5875cd8222ae87eac
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-06-30 19:31:56 +0200

    Discord js v13 - index

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client, Intents} = require('discord.js');
+const client = new Client({intents: [Intents.ALL]});
 
-client.on('ready', () => console.log('On'));
+const fs = require('fs')
+const config = require('./config');
 
-client.ws.on('INTERACTION_CREATE', async interaction => {
-    const command = interaction.data.name.toLowerCase();
+client.queue = new Map();
 
-    if (command == `pong`) {
-        client.api.interactions(interaction.id, interaction.token).callback.post({
-            data: {
-                type: 5,
-            }
-        })
-        client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({
-            data: {
-                content: `Ping`
-            }
-        })
+client.on('ready', () => console.log('Connecter !'));
+
+client.login(config.token);
+
+
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
+
+client.on('interaction', async (interaction) => {
+    const guild = client.guilds.cache.get(interaction.guild.id)
+    const member = guild.members.cache.get(interaction.member.user.id);
+    const voiceChannel = member.voice.channel;
+
+try {
+    client.connection = ({
+        channelId: voiceChannel.id,
+        guildId: guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        selfDeaf: false,
+        selfMute: false,
+    });
+}catch (err) {
+   return interaction.reply('Vous n\'est pas dans un channel vocal')
+}
+    for (const file of commandFiles) {
+        let cmd = require(`./commands/${file}`);
+
+        if (cmd.name === interaction.commandName) {
+            command = cmd;
+        }
+    }
+    if (command !== null) {
+        command.execute(interaction, client);
     }
 })
 
-
-
-client.login(token);
 
