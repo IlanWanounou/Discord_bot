1a2aa4d7d4fe9921d223c5708420f5530cb134b3
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-06-30 20:02:58 +0200

    Fix bug

--- commands/join.js
+++ commands/join.js
@@ -3,7 +3,7 @@ const { joinVoiceChannel } = require('@discordjs/voice')
 module.exports = {
     name: 'join',
     async execute(interaction) {
-        const connection = interaction.client.connection=joinVoiceChannel;
+        const connection = interaction.client.connection;
 
             if(connection) return interaction.reply('Salon bien rejoin')
 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {Client, Intents} = require('discord.js');
 const client = new Client({intents: [Intents.ALL]});
 
+const { joinVoiceChannel } = require('@discordjs/voice')
+
 const fs = require('fs')
 const config = require('./config');
 
@@ -20,7 +22,7 @@ client.on('interaction', async (interaction) => {
     const voiceChannel = member.voice.channel;
 
 try {
-    client.connection = ({
+    client.connection = joinVoiceChannel ({
         channelId: voiceChannel.id,
         guildId: guild.id,
         adapterCreator: voiceChannel.guild.voiceAdapterCreator,
