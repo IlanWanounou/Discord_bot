750f2a1fbe74cdb080b49a3f384b436dee6fe3de
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-07-03 22:48:06 +0200

    Revoie un msg si il n'y a pas de musique

--- commands/loop.js
+++ commands/loop.js
@@ -2,6 +2,7 @@ module.exports =  {
     name:'loop',
         execute(interaction) {
             const serverQueue = interaction.client.queue.get(interaction.guild.id);
+            if(!serverQueue) return interaction.reply('Pas de musique en cour');
             serverQueue.loop =!serverQueue.loop;
             return interaction.reply(`loop :  ${serverQueue.loop ?  `**activer**` : `**désactiver**` } `);
 
