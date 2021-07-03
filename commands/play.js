2d4f0d3ddc232579e06d0efc63346c2e3b5f7251
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-07-03 22:54:51 +0200

    Création play

--- commands/play.js
+++ commands/play.js
@@ -1,83 +1,76 @@
-const {MessageEmbed} = require('discord.js');
-const ytdl = require('ytdl-core');
+const {createAudioResource, AudioPlayerStatus} = require('@discordjs/voice');
+const ytdl = require('ytdl-core')
+
 module.exports = {
-	name: 'play',
-	async execute(message, args) {
-		const queue = message.client.queue;
-		const serverQueue = message.client.queue.get(message.guild.id);
-        let channelCheck = message.member.voice.channel;
-        if(!channelCheck){
-            return message.channel.send("Tu dois rejoindre un salon vocal avant de faire cette commande.");
-        }else{
-           
-            const songInfo = await ytdl.getInfo(args[0]);
- 
-            let song = {
-                title: songInfo.videoDetails.title,
-                url: songInfo.videoDetails.video_url,
-                tempsMusique: songInfo.videoDetails.lengthSeconds
-                
+    name: 'play',
+    async execute(interaction) {
+        interaction.reply('Je joue : . . . .')
+
+        const connection = interaction.client.connection;
+        const queue = interaction.client.queue;
+        const guildQueue = interaction.client.queue.get(interaction.guild.id);
+        const songInfo = await ytdl.getInfo(interaction.options.get('url').value);
+        const player = interaction.client.player();
+
+        let song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            tempsMusique: songInfo.videoDetails.lengthSeconds
+        };
+        if (song.tempsMusique > 10800) return interaction.editReply('Imposible de lancer une musique > 3 heures.');
+
+        if (!guildQueue) {
+            const queueConstructor = {
+                connection: null,
+                songs: [],
+                volume: 10,
+                loop: false,
+                playing: true
             };
- 
-            if(!serverQueue){
-                const queueConstructor = {
-                    txtChannel: message.channel,
-                    vChannel: channelCheck,
-                    connection: null,
-                    songs: [],
-                    volume: 10,
-                    loop: false,
-                    playing: true
-                };
-                queue.set(message.guild.id, queueConstructor);
- 
-                queueConstructor.songs.push(song);
- 
-                try{
-                    let connection = await channelCheck.join();
-                    queueConstructor.connection = connection;
-                    play(message.guild, queueConstructor.songs[0]);
-                }catch (err){
-                    console.error(err);
-                    queue.delete(message.guild.id);
-                    return message.channel.send(`erreur : ${err}`);
-                }
-            }else{
-                serverQueue.songs.push(song);
-                return message.channel.send(`${song.title} à été ajouter à la liste`);
+            queue.set(interaction.guild.id, queueConstructor);
+            queueConstructor.songs.push(song);
+
+            try {
+                queueConstructor.connection = connection;
+                play(queueConstructor.songs[0]);
+            } catch (err) {
+                console.error(err);
+                queue.delete(interaction.guild.id);
             }
-        }
-    async function play(guild, song){
-        const serverQueue = queue.get(guild.id);
-        if(!song){
-         setTimeout(() => {
-                serverQueue.vChannel.leave();   
-            }, 300000); // leave le salon apres 5min sans musique.
-            queue.delete(guild.id);
-            
-            return;
+
+        } else {
+            console.log(guildQueue.songs)
+
+            guildQueue.songs.push(song);
+            return interaction.editReply(`${song.title} à été ajouter à la liste`);
         }
 
-         dispatcher = await serverQueue.connection
-            .play(ytdl(song.url))
-            .on('finish', () =>{
-                if(!serverQueue.loop)serverQueue.songs.shift();
-                play(guild, serverQueue.songs[0]);
-            })
-            
-			serverQueue.connection.on("disconnect", () => queue.delete(guild.id));
-            const embed = new MessageEmbed()
-            
-            .setAuthor("Musique en cours :")
-            .setTitle(`${serverQueue.songs[0].title}`)
-                .setURL(`${serverQueue.songs[0].url}`)
-                
-                 serverQueue.txtChannel.send(embed);
-            
+        function play(song) {
+            if (!song) {
+                setTimeout(() => {
+                    connection.destroy();
+                    queue.delete(interaction.client.queue);
+                    }, 300000); // leave le salon apres 5min sans musique.
+
+            } else {
+                const serverQueue = queue.get(interaction.guild.id);
+                let resource = createAudioResource(ytdl(song.url));
+                player.play(resource);
+                connection.subscribe(player)
+                player.on(AudioPlayerStatus.Idle, () => {
+                    if (!serverQueue.loop) serverQueue.songs.shift();
+                    play(serverQueue.songs[0]);
+                })
+                serverQueue.connection.on('disconnect', () => {
+                    connection.destroy();
+                    queue.delete(interaction.client.queue);
+
+                })
+                return interaction.editReply(`Je joue : ${song.title}`)
+            }
+
+
         }
-        if (message.guild.me.voice.channel) { 
-            message.guild.me.voice.setSelfDeaf(true);
-        };
-	}
+    }
 }
 
