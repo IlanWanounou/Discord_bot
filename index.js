d49ea3fcb343f2da3f2200ff2b43ff84263c28b9
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-07-07 20:32:55 +0200

    Changement pour la connexion à un channel vocal. Log des commandes faite.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const {Client, Intents} = require('discord.js');
 const client = new Client({intents: [Intents.ALL]});
 
-const { joinVoiceChannel, createAudioPlayer } = require('@discordjs/voice')
+const { createAudioPlayer } = require('@discordjs/voice')
 
 const fs = require('fs')
 const config = require('./config');
 
 client.queue = new Map();
 client.player = createAudioPlayer;
+client.connection;
 
 client.on('ready', () => console.log('Connecter !'));
 
@@ -18,22 +19,9 @@ const commandFiles = fs
     .filter((file) => file.endsWith(".js"));
 
 client.on('interaction', async (interaction) => {
-    const guild = client.guilds.cache.get(interaction.guild.id)
+    const guild = client.guilds.cache.get(interaction.guild.id);
     const member = guild.members.cache.get(interaction.member.user.id);
-    const voiceChannel = member.voice.channel;
 
-try {
-    client.connection = joinVoiceChannel ({
-        channelId: voiceChannel.id,
-        guildId: guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-        selfDeaf: true,
-        selfMute: false,
-    });
-}catch (err) {
-    console.error(err)
-   return interaction.reply('Vous n\'est pas dans un channel vocal')
-}
     for (const file of commandFiles) {
         let cmd = require(`./commands/${file}`);
 
@@ -43,6 +31,7 @@ try {
     }
     if (command !== null) {
         command.execute(interaction, client);
+        console.log(`${member.user.username} à utliser la commande ${interaction.commandName} sur le serveur :  ${guild.name}`)
     }
 })
 
