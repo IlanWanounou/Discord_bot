c59768d227251acab79ce77683ebbffcfcb2659f
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-07-07 21:22:56 +0200

    Refonte sur la connexion au vocal

--- commands/join.js
+++ commands/join.js
@@ -1,11 +1,20 @@
-const { joinVoiceChannel } = require('@discordjs/voice')
-
+const { joinVoiceChannel } = require('@discordjs/voice');
 module.exports = {
     name: 'join',
-    async execute(interaction) {
-        const connection = interaction.client.connection;
+    async execute(interaction, client) {
 
-            if(connection) return interaction.reply('Salon bien rejoin')
+        const guild = client.guilds.cache.get(interaction.guild.id);
+        const member = guild.members.cache.get(interaction.member.user.id);
+        const voiceChannel = member.voice.channel;
+
+        const connection= interaction.client.connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        });
+
+        if(connection) return interaction.reply(`J\'ai rejoin ${voiceChannel.name}`)
+
 
     }
 
