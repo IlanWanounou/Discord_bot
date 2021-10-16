c5ecd04ecf4d363732e5ce6264fbd3d9b996d4a4
IlanWanonou <wanounouilan@gmail.com>
2021-10-16 22:58:26 +0200

    commande join

---
+++ commands/join.js
@@ -0,0 +1,25 @@
+const {joinVoiceChannel} = require('@discordjs/voice')
+module.exports = {
+    name: 'join',
+    description: 'rejoins le serveur vocal ou est connecter le client.',
+    async execute(interaction, client) {
+        const guild = client.guilds.cache.get(interaction.guild.id);
+        const member = guild.members.cache.get(interaction.member.user.id);
+        const voiceChannel = member.voice.channel;
+        if(!voiceChannel) return interaction.reply('Vous n\'est pas dans un salon vocal.')
+        if(guild.me.voice.channel) return interaction.reply('Je suis déjà dans un salon vocal.')
+        else {
+            join(interaction, guild, voiceChannel)
+            interaction.reply(`J'ai rejoin ${voiceChannel.name}`)
+        }
+        function join(interaction, guild, voiceChannel) {
+
+            interaction.client.connection = joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: guild.id,
+                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+            });
+    }
+}
+}
+
