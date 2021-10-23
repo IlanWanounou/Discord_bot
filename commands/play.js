2c949bcd9bfa3b907f884785a83472241a0e2366
Ilan Wanounou <wanounouilan@gmail.com>
2021-10-24 00:37:08 +0200

    musique avec queue (non fini)

--- commands/play.js
+++ commands/play.js
@@ -1,27 +1,71 @@
-const {createAudioResource, joinVoiceChannel, createAudioPlayer} = require('@discordjs/voice');
-const play = require('play-dl');
+const {createAudioResource, createAudioPlayer} = require('@discordjs/voice');
+const playYT = require('play-dl');
+const ytdl = require('ytdl-core');
 const {joinC} = require('../includes/join');
+//const {play} = require('../includes/play');
+
 module.exports = {
     name: 'play',
     async execute(interaction, client) {
-        const guild = client.guilds.cache.get(interaction.guild.id);
-        const member = guild.members.cache.get(interaction.member.user.id);
-        const voiceChannel = member.voice.channel;
+        const queue = client.queue;
+        const guildQueue = interaction.client.queue.get(interaction.guild.id);
         const url = interaction.options.get('url').value;
-        let yt_info = await play.search(url, { limit : 1 })
-        interaction.reply(`Je joue ${yt_info[0].title}`)
+        interaction.reply('Je joue ...')
 
-        try {
-        joinC(interaction, guild, voiceChannel);
-        const player = await createAudioPlayer();
-        let stream = await play.stream(yt_info[0].url)
-            let resource = await createAudioResource(stream.stream);
+        let songInfo = {
+            url:(await ytdl.getInfo(url)).videoDetails.video_url,
+            title: (await ytdl.getInfo(url)).videoDetails.title,
+          //  tempsMusique: (await ytdl.getInfo(url)).videoDetails.lengthSeconds
+        }
+        if(!guildQueue) {
+            const queueConstructor = {
+                songs: [],
+                volume: 10,
+                loop: false,
+            };
+            queue.set(interaction.guild.id, queueConstructor);
+            queueConstructor.songs.push(songInfo);
+            try {
+                const guild = client.guilds.cache.get(interaction.guild.id);
+                const member = guild.members.cache.get(interaction.member.user.id);
+                const voiceChannel = member.voice.channel;
 
-        await  player.play(resource);
-         await interaction.client.connection.subscribe(player);
-        } catch (e) {
-            console.log(e);
+                joinC(interaction, guild, voiceChannel);
+                const player = await createAudioPlayer();
+                let stream = await playYT.stream(queueConstructor.songs[0].url)
+                let resource = await createAudioResource(stream.stream);
+                await interaction.editReply('Je joue'+queueConstructor.songs[0].title)
+
+                await  player.play(resource);
+                await interaction.client.connection.subscribe(player);
+            }catch (e) {
+                console.error(e)
+                queue.delete(interaction.guild.id);
+            }
+        } else {
+
+            return interaction.editReply(songInfo.title +' a été ajouté à la liste');
         }
 
+        /*
+         const url = interaction.options.get('url').value;
+
+
+
+         let yt_info = await play.search(url, { limit : 1 });
+
+         try {
+         joinC(interaction, guild, voiceChannel);
+         const player = await createAudioPlayer();
+         let stream = await play.stream(yt_info[0].url);
+         let resource = await createAudioResource(stream.stream);
+
+         await  player.play(resource);
+          await interaction.client.connection.subscribe(player);
+         } catch (e) {
+             console.log(e);
+         }
+
+     }*/
     }
 }
