51d4a9571e0041150fc9b089a6ba053be2846589
Ilan Wanounou <wanounouilan@gmail.com>
2021-11-27 00:05:11 +0100

    Ajout command handler

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
-import { Connection } from './Connection';
+import { Connection } from './src/Connection';
+import {Interaction} from "discord.js";
+import * as fs from 'fs';
 
-let client = new Connection();
+const files = fs.readdirSync('./src/commands').filter((file:string) =>
+    file.endsWith('.ts'));
 
+let client = new Connection()
 client.Start();
+
+client.getClient.on('interactionCreate', async (interaction:Interaction) => {
+    let command = null;
+    for (let file of files) {
+        file=require(`./src/commands/${file}`)
+        if(file.name===interaction.commandName){
+            command=file
+        }
+    }
+    if(command !== null) {
+        command.execute(interaction, client.getClient)
+    }
+})
