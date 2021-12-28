24b5cdff57949622b5f90e4703930d30f62bc16c
Ilan Wanounou <wanounouilan@gmail.com>
2021-12-28 16:59:57 +0100

    Réorganisation de l'application

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 import { Connection } from './src/Connection';
-import {Interaction} from "discord.js";
+import {ContextMenuInteraction} from 'discord.js';
 import * as fs from 'fs';
 
-const files = fs.readdirSync('./src/commands').filter((file:string) =>
+const files:any = fs.readdirSync('./src/commands').filter((file:string) =>
     file.endsWith('.ts'));
 
 let client = new Connection()
 client.Start();
 
-client.getClient.on('interactionCreate', async (interaction:Interaction) => {
+client.getClient.on('interactionCreate', async (interaction:ContextMenuInteraction) => {
     let command = null;
     for (let file of files) {
         file=require(`./src/commands/${file}`)
         if(file.name===interaction.commandName){
-            command=file
+            command=file;
         }
     }
     if(command !== null) {
