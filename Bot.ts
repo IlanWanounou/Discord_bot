a9eba03ce49167012321642cf36faa57db438d18
Ilan Wanounou <wanounouilan@gmail.com>
2022-10-24 17:06:17 +0200

    Add env file

--- Bot.ts
+++ Bot.ts
@@ -4,7 +4,8 @@ console.log("Bot is starting...");
 
 const client = new Client({
     intents: []
+client.login(process.env.TOKEN);
 });
-client.login("NzgxOTIwNTE0NDQ4OTQ5MzIw.GrEEng.frRi4arFOhxhh0ed-nLN3yWB7Tt29TEl15OL34");
+
 
 console.log(client);
